c9d55cebee60d06d13812954e7873edfa930abfe
Hayate Nakano <nakanodesu@gmail.com>
2022-03-21 10:31:33 +0900

    config wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,27 @@
-local wezterm = require 'wezterm';
+local wezterm = require 'wezterm'
+
+local ssh_domains = {}
+
+for host, config in pairs(wezterm.enumerate_ssh_hosts()) do
+    if host == "xps" then
+        table.insert(ssh_domains, {
+            name = host,
+            remote_address = config["hostname"],
+            username = config["user"],
+            ssh_option = {
+                identityfile = config["identityfile"]
+            },
+            remote_wezterm_path = "/home/linuxbrew/.linuxbrew/bin/wezterm",
+            assume_shell = "Posix",
+        })
+    end
+end
+
 return {
     default_prog = {"zsh", "-lc", [[exec fish]]},
     font = wezterm.font("Cica"),
     font_size = 15,
     color_scheme = "Dracula",
+    window_background_opacity = 0.9,
+    ssh_domains = ssh_domains,
 }
