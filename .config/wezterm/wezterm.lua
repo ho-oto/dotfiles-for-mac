e9a3d486c3ec97f6a9cceece41a70ccc00323f29
Hayate Nakano <nakanodesu@gmail.com>
2022-05-08 15:12:11 +0900

    add eikana

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -21,6 +21,7 @@ return {
     default_prog = {"zsh", "-lc", [[exec fish]]},
     font = wezterm.font("Cica"),
     font_size = 15,
+    tab_bar_at_bottom = true,
     color_scheme = "Dracula",
     window_background_opacity = 0.9,
     ssh_domains = ssh_domains,
