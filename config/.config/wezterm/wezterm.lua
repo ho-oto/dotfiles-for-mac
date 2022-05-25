736d6db9aceffffe35f780a53ee9bcefce0a3907
Hayate Nakano <nakanodesu@gmail.com>
2022-05-25 17:51:37 +0900

    up

--- config/.config/wezterm/wezterm.lua
+++ config/.config/wezterm/wezterm.lua
@@ -19,10 +19,33 @@ end
 
 return {
     default_prog = {"zsh", "-lc", [[exec fish]]},
-    font = wezterm.font("Cica"),
+    font = wezterm.font_with_fallback({
+        "Cica",
+        "JetBrains Mono",
+        "Fira Code",
+    }),
     font_size = 15,
     tab_bar_at_bottom = true,
     color_scheme = "Dracula",
     window_background_opacity = 0.9,
     ssh_domains = ssh_domains,
+    leader = {key="q", mods="CTRL", timeout_milliseconds=1000},
+    keys = {
+        {key="|", mods="LEADER", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
+        {key="-", mods="LEADER", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
+        {key="c", mods="LEADER", action=wezterm.action{SpawnTab="CurrentPaneDomain"}},
+        {key="x", mods="LEADER", action=wezterm.action{CloseCurrentTab={confirm=false}}},
+
+        {key="LeftArrow", mods="LEADER", action=wezterm.action{ActivatePaneDirection="Left"}},
+        {key="RightArrow", mods="LEADER", action=wezterm.action{ActivatePaneDirection="Right"}},
+        {key="UpArrow", mods="LEADER", action=wezterm.action{ActivatePaneDirection="Up"}},
+        {key="DownArrow", mods="LEADER", action=wezterm.action{ActivatePaneDirection="Down"}},
+
+        {key="LeftArrow", mods="LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Left", 1}}},
+        {key="RightArrow", mods="LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Right", 1}}},
+        {key="UpArrow", mods="LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Up", 1}}},
+        {key="DownArrow", mods="LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Down", 1}}},
+
+        {key="q", mods="LEADER|CTRL", action=wezterm.action{SendKey={key="q", mods="CTRL"}}},
+    }
 }
