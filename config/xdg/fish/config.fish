c27d780017c63a3013eeed05958586a5401bee8a
Hayate Nakano <nakanodesu@gmail.com>
2024-02-11 21:10:00 +0900

    up

--- config/xdg/fish/config.fish
+++ config/xdg/fish/config.fish
@@ -38,22 +38,5 @@ end
 
 if status is-interactive
     starship init fish | source
-    pyenv init - | source
     zoxide init fish | source
-end
-
-function brew
-    set --export --local PATH $PATH
-    if type -q pyenv
-        while contains (pyenv root)/shims $PATH
-            set --erase PATH[(contains --index (pyenv root)/shims $PATH)]
-        end
-    end
-    if type -q cargo-install-update-config
-        set CARGO_PATH (dirname (which cargo-install-update-config))
-        while contains $CARGO_PATH $PATH
-            set --erase PATH[(contains --index $CARGO_PATH $PATH)]
-        end
-    end
-    command brew $argv
 end
